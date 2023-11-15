2fcf94132634df58060dab57b8242315fcdb659a
Travis Richardson <tlrichar@gmail.com>
2023-11-15 13:38:44 -0500

    1st update. this is working on sepolia using Remix, but not using HH/Tendelry

---
+++ hardhat/hardhat.config.ts
@@ -0,0 +1,67 @@
+import { HardhatUserConfig, task } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+import * as tenderly from "@tenderly/hardhat-tenderly";
+import * as dotenv from "dotenv";
+import "hardhat-deploy";
+
+dotenv.config();
+
+
+const providerApiKey = process.env.ALCHEMY_API_KEY || "oKxs-03sij-U_N0iOlrSsZFr29-IqbuF";
+const tenderlyAccessKey = process.env.TENDERLY_ACCESS_KEY || ""; 
+// If not set, it uses the hardhat account 0 private key.
+const deployerPrivateKey =
+  process.env.DEPLOYER_PRIVATE_KEY ?? "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80";
+// If not set, it uses ours Etherscan default API key.
+const etherscanApiKey = process.env.ETHERSCAN_API_KEY || "DNXJA8RX2Q3VZ4URQIWP7Z68CJXQZSC6AW";
+
+tenderly.setup({ automaticVerifications: false });
+
+const config: HardhatUserConfig = {
+  solidity: "0.8.19",
+  defaultNetwork: "tenderly",
+  networks: {
+    hardhat: {
+      forking: {
+        url: `https://eth-mainnet.alchemyapi.io/v2/${providerApiKey}`,
+        enabled: process.env.MAINNET_FORKING_ENABLED === "true",
+      },
+    },
+    tenderly: {
+      url: "https://rpc.vnet.tenderly.co/devnet/erc20tokentransfer/142549d2-3369-4003-86b7-dc1f5515d181",
+      chainId: 11155111,
+    },
+    sepolia: {
+      url: `https://eth-sepolia.g.alchemy.com/v2/${providerApiKey}`,
+      accounts: [deployerPrivateKey],
+    },
+    sepoliaTenderly: {
+      url: `https://sepolia.gateway.tenderly.co/${tenderlyAccessKey}`,
+      accounts: [deployerPrivateKey],
+      chainId: 11155111,
+    },
+  },
+  tenderly: {
+    username: "tlrichar",
+    project: "project",
+  },
+  verify: {
+    etherscan: {
+      apiKey: `${etherscanApiKey}`,
+    },
+  }
+};
+
+//do later if needed. 
+/* task("verifyExistingMS", "Verifies deployed MultiSigWallet instance")
+  .addParam("address")
+  .setAction(async (args, hre) => {
+    await hre.run("compile");
+    console.log("Verifying MS", args.address);
+    await hre.tenderly.verify({
+      name: "MultiSigWallet",
+      address: args.address,
+    });
+  }); */
+
+export default config;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,26 +1,98 @@
+import { ethers } from "ethers";
+import { useState } from 'react';
 import { ConnectButton } from '@rainbow-me/rainbowkit'
 import type { NextPage } from 'next'
 import Head from 'next/head'
+import token from '../pages/abi/Token.json';
+import * as dotenv from "dotenv";
+dotenv.config();
+
+
+const tokenABI = token.Token.abi; 
+const tokenAddress = "0x1756a8D1f35CC5B97cc1237F82254CF466dbC83f" //Sepolia Deployment
+//const tokenAddress = "0xC5b3FE820407650830cA5ce4Ed524481D732Ed49" //Tenderly Deployment
+
+// Tenderly DevNet
+//const provider = new ethers.providers.JsonRpcProvider("https://rpc.vnet.tenderly.co/devnet/erc20tokentransfer/142549d2-3369-4003-86b7-dc1f5515d181");
+// Sepolia
+const provider = new ethers.providers.JsonRpcProvider(process.env.TENDERLY_SEPOLIA_RPC);
+
+const signer = provider.getSigner();
+
+const tokenContract = new ethers.Contract(tokenAddress, tokenABI, signer);
+const senderAddress = "0xC305f4b9925b9eC6b3D0FCC42B7b22F1245A5011";
+const receiverAddress = "0xdb623c0F74d4ed5af4B254327147c4aC7E5d3fAC";
 
 const Home: NextPage = () => {
+  const [isTransferring, setIsTransferring] = useState(false);
+  const sendAmount = ethers.utils.parseUnits("1", 18);
+
+  const handleTransferClick = async () => {
+    setIsTransferring(true);
+
+    try {
+
+      // Check balances before the Send
+      let senderBalance = await tokenContract.balanceOf(senderAddress);
+      let receiverBalance = await tokenContract.balanceOf(receiverAddress);
+      console.log("Sender balance before transfer:", ethers.utils.formatUnits(senderBalance, 18));
+      console.log("Receiver balance before transfer:", ethers.utils.formatUnits(receiverBalance, 18));
+     
+      // Approve the senderAddress to spend tokens
+      let approveTx = await tokenContract.approve(senderAddress, sendAmount);
+      await approveTx.wait();
+      console.log("Approved for transfer.");
+
+      // Check Allowance
+      const allowance = await tokenContract.allowance(senderAddress, senderAddress); // Owner = Spender
+      console.log("Allowance Granted:", ethers.utils.formatUnits(allowance, 18));
+
+      // Send the tokens
+      console.log("Sending...", ethers.utils.formatUnits(sendAmount, 18));
+      const tx = await tokenContract.transferFrom(senderAddress, receiverAddress, sendAmount);
+      await tx.wait();
+      console.log("Send complete.");
+
+      // check balances after the send
+      senderBalance = await tokenContract.balanceOf(senderAddress);
+      receiverBalance = await tokenContract.balanceOf(receiverAddress);
+      console.log("Sender balance after transfer:", ethers.utils.formatUnits(senderBalance, 18));
+      console.log("Receiver balance after transfer:", ethers.utils.formatUnits(receiverBalance, 18));
+
+    } catch (error) {
+      console.error("Transfer failed:", error);
+    } finally {
+      setIsTransferring(false);
+    }
+  }
+
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
       <Head>
-        <title>Create Rainbowkit App</title>
+        <title>Token Transfer</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className="">
         <div className="flex flex-col items-center">
           <h1 className="text-4xl font-bold text-center">
             <span className="to-blue-500 bg-clip-text">
-              Get Started with Rainbow Kit 🌈🧰
+              Token Transfer 🧰
             </span>
           </h1>
           <h3 className="mt-2 text-gray-400">
-            Pre-installed NextJS (Typescript), TailwindCSS, Rainbowkit, Wagmi
+            Transfer Your Tokens on a Tenderly DevNet
           </h3>
-          <div className="mt-4">
-            <ConnectButton />
+          <div className="mt-4 flex">
+        {/*}    <ConnectButton /> */}
+            <button
+          className={`ml-2 bg-purple-500 hover:bg-purple-700 text-white font-bold py-2 px-12 rounded-xl ${
+            isTransferring ? 'opacity-50 cursor-not-allowed' : ''
+          }`}
+          onClick={handleTransferClick}
+          disabled={isTransferring}
+        >
+          {isTransferring ? 'Transferring...' : 'Transfer Tokens'}
+            </button>
           </div>
         </div>
       </main>
